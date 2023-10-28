58f461ee1e66a944f49c96196906b09a1e971b75
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-10-28 21:13:00 +0300

    done tast-01

--- js/task-01.js
+++ js/task-01.js
@@ -1,4 +1,26 @@
 // Отримуємо посилання на елемент ul#categories
-const categoriesList = document.querySelector('ul');
-console.dir(categoriesList);
+const categoriesList = document.querySelector('#categories');
   
+// Отримуємо всі елементи li.item в межах ul#categories
+const categoryItems = categoriesList.querySelectorAll('li.item');
+    
+//Консолимо загальну кількість категорій
+    console.log(`Number of categories: ${categoryItems.length}.`);
+
+// Використовуємо forEach для перебору кожного елемента li.item
+categoryItems.forEach((categoryItem) => {
+
+    // Отримуємо текст заголовку (тегу <h2>) кожної категорії 
+    const categoryName = categoryItem.querySelector('h2').textContent;
+
+    // Отримуємо всі елементи li в межах кожної категорії
+    const categoryElements = categoryItem.querySelectorAll('ul > li');
+
+    // Створюємо об'єкт для виведення в консоль
+    const categoryInfo = {
+        Category: categoryName,
+        Elements: categoryElements.length,
+    };
+
+    console.log(categoryInfo);
+});
