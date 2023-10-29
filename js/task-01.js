055596d5cc178ae3c76ea98cc50f45bead8a09c4
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-10-29 15:05:35 +0200

    done task-02 + some css-styles

--- js/task-01.js
+++ js/task-01.js
@@ -1,9 +1,10 @@
-// Отримуємо посилання на елемент ul#categories
+// Отримуємо посилання на елемент ul#categories + додаємо клас list
 const categoriesList = document.querySelector('#categories');
+categoriesList.classList.add("list");
   
 // Отримуємо всі елементи li.item в межах ul#categories
 const categoryItems = categoriesList.querySelectorAll('li.item');
-    
+
 //Консолимо загальну кількість категорій
     console.log(`Number of categories: ${categoryItems.length}.`);
 
@@ -12,6 +13,14 @@ categoryItems.forEach((categoryItem) => {
 
     // Отримуємо текст заголовку (тегу <h2>) кожної категорії 
     const categoryName = categoryItem.querySelector('h2').textContent;
+
+    // Знайдемо всі внутрішні списки ul
+  const subcategoryLists = categoryItem.querySelectorAll('ul');
+
+    // Пройдемося по кожному внутрішньому списку та змінимо маркери на квадрати
+    subcategoryLists.forEach((ul) => {
+        ul.style.listStyleType = 'square';
+      });
 
     // Отримуємо всі елементи li в межах кожної категорії
     const categoryElements = categoryItem.querySelectorAll('ul > li');
@@ -24,3 +33,4 @@ categoryItems.forEach((categoryItem) => {
 
     console.log(categoryInfo);
 });
+
--- js/task-02.js
+++ js/task-02.js
@@ -2,7 +2,54 @@ const ingredients = [
   'Potatoes',
   'Mushrooms',
   'Garlic',
-  'Tomatos',
+  'Tomatoes',
   'Herbs',
   'Condiments',
 ];
+
+// // Отримуємо посилання на елемент ul#ingredients + додаємо клас list
+// const list = document.querySelector('#ingredients')
+// // console.dir(list);
+// list.classList.add("list");
+
+// // Створюємо фрагмент (Цей крок створює порожній DocumentFragment, який використовуватиметься для оптимізованого додавання багатьох елементів до DOM за одну операцію.)
+// const fragment = document.createDocumentFragment(); 
+
+// //Перебираємо масив ingredients, створюємо та налаштовуємо кожен елемент <li>:
+// ingredients.forEach(ingredient => {
+
+// const li = document.createElement('li'); // Створюємо новий елемент <li>
+
+// li.textContent = ingredient; // Додаємо текстовий вміст (інгредієнт) в <li>
+
+// li.classList.add("item"); //Додаємо клас "item" до <li>
+
+// fragment.append(li) //Додаємо кожен <li> до DocumentFragment
+// });
+
+// //Вставляємо DocumentFragment з усіма <li> елементами в список <ul> (Всі елементи вставляються за одну операцію, що робить код більш продуктивним.)
+// list.append(fragment); 
+
+
+//---------------------------АБО------------------------------//
+
+const parentElement = document.getElementById("ingredients");
+parentElement.classList.add("list");
+
+//Перетворюємо масив ingredients в масив listIngredients
+const listIngredients = ingredients.map(ingredient => {
+
+  //Для кожного елемента ingredient ми створюємо новий <li> елемент + додаємо клас 'item'
+  const listIngredient = document.createElement("li");
+  listIngredient.classList.add("item");
+
+  //Встановлюємо текстовий вміст
+  listIngredient.textContent = ingredient;
+
+  //Повертаємо цей елемент як результат map.
+  return listIngredient;
+});
+
+//Вставляємо всі <li> елементи в батьківський елемент parentElement за одну операцію:
+parentElement.append(...listIngredients);
+
