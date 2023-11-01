e8b3323226ddc4f48972566339c7182448c761b5
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-11-01 22:39:20 +0200

    done task-05

--- js/task-05.js
+++ js/task-05.js
@@ -1,2 +1,20 @@
 const navigation = document.querySelector('a');
 navigation.classList.add("link");
+
+
+
+const nameInput = document.querySelector('#name-input');
+
+const nameOutput = document.querySelector('#name-output')
+
+nameInput.addEventListener('input', onInput);
+
+function onInput(evt) {
+    // evt.preventDefault() (скидаємо дефолтну/стандартну поведінку)
+
+
+    const inputValue = nameInput.value;
+    // console.log(inputValue);
+
+    nameOutput.textContent = inputValue || 'Anonymous'
+}
