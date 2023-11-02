457d102a30e3e154a155637169e787d1945a9642
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-11-02 21:44:47 +0200

    done task-07

--- js/task-07.js
+++ js/task-07.js
@@ -1,2 +1,11 @@
 const navigation = document.querySelector('a');
 navigation.classList.add("link");
+
+
+const textInput = document.querySelector('#font-size-control')
+const textElement = document.querySelector('#text')
+
+textInput.addEventListener('input', (evt) => {
+    const fontSize = textInput.value + 'px';
+    textElement.style.fontSize = fontSize;
+});
