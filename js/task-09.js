6a07044d8ab34fda95a8fe67bff28373f8a49b4e
OlichkaKhamuliak <olkoboyko97@gmail.com>
2023-11-03 16:39:25 +0200

    done task-09

--- js/task-09.js
+++ js/task-09.js
@@ -6,3 +6,24 @@ function getRandomHexColor() {
 
 const navigation = document.querySelector('a');
 navigation.classList.add("link");
+
+const body = document.querySelector('body');
+body.style.fontFamily = 'Edu TAS Beginner';
+body.style.fontStyle = 'italic';
+
+
+const btnChangeColor = document.querySelector('.change-color');
+btnChangeColor.classList.add('btn');
+
+const span = document.querySelector('.color');
+span.style.fontSize = '50px';
+span.style.fontFamily = 'Edu TAS Beginner';
+
+const handleClick = (event) => {
+  const color = getRandomHexColor();
+  body.style.backgroundColor = color;
+  span.textContent = color;
+};
+
+btnChangeColor.addEventListener("click", handleClick);
+
